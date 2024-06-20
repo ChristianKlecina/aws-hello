24a87914f9de74ae21c815724cd5dc99b127b52b
ChristianKlecina <djfntt@yahoo.com>
2024-06-20 23:20:06 +0200

    i hope so

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -4,8 +4,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,30 +13,25 @@ import java.util.Map;
 	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent , Map<String, Object>> {
+public class HelloWorld implements RequestHandler<Map<String, Object> , Map<String, Object>> {
 
 
 
 
-	public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent requestData, Context context) {
+	public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
 
 
 		Map<String, Object> resultMap = new HashMap<>();
-		try {
-			// Main logic
-			System.out.println("Hello from lambda");
-			resultMap.put("statusCode", 200);
-			resultMap.put("message", "Hello from Lambda");
+		Map<String, Object> response = new HashMap<>();
+		String path = (String) input.get("path");
+		String method = (String) input.get("httpMethod");
 
-			// Retrieve AWS function URL
-			String functionArn = context.getInvokedFunctionArn();
-			String functionUrl = "https://" + functionArn.split(":")[5] + ".execute-api." + context.getAwsRequestId().split(":")[3] + ".amazonaws.com";
-			resultMap.put("functionUrl", functionUrl);
-		} catch (Exception e) {
-			// Handle error
-			resultMap.put("statusCode", 400);
-			resultMap.put("message", "Bad request syntax or unsupported method. Request path: "
-					+ requestData.getRequestContext().getHttp().getPath() + ". HTTP method: " + requestData.getRequestContext().getHttp().getMethod());
+		if ("/hello".equals(path)) {
+			response.put("statusCode", 200);
+			response.put("message", "Hello from Lambda");
+		} else {
+			response.put("statusCode", 400);
+			response.put("message", "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method);
 		}
 
 		return resultMap;
