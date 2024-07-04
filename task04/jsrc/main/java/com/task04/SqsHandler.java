357769ccfb18d51ab8400c970445c9f971125c64
ChristianKlecina <djfntt@yahoo.com>
2024-07-04 18:28:37 +0200

    maybe works

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -1,41 +1,44 @@
 package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-
-import com.syndicate.deployment.annotations.resources.*;
-import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @LambdaHandler(lambdaName = "sqs_handler",
-    roleName = "sqs_handler-role",
-    isPublishVersion = false,
-       runtime =  DeploymentRuntime.JAVA11,
-    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "sqs_handler-role",
+        isPublishVersion = false,
+        timeout = 10
+ 
 )
 @SqsTriggerEventSource(
-       batchSize = 10,
-       targetQueue = "async_queue"
+        targetQueue = "async_queue",
+        batchSize = 10
 )
 @DependsOn(
-       name = "async_queue",
-       resourceType = ResourceType.SQS_QUEUE
+        name = "async_queue",
+        resourceType = ResourceType.SQS_QUEUE
 )
 public class SqsHandler implements RequestHandler<SQSEvent, String> {
-
-	private static final Logger logger = LoggerFactory.getLogger(SqsHandler.class);
-
-	@Override
-	public String handleRequest(SQSEvent event, Context context) {
-		for (SQSEvent.SQSMessage message : event.getRecords()) {
-			logger.info("Message Body: {}", message.getBody());
-		}
-		return "Messages processed successfully";
-	}
+ 
+    private static final Logger logger = LogManager.getLogger(SqsHandler.class);
+ 
+    @Override
+    public String handleRequest(SQSEvent sqsEvent, Context context) {
+        LambdaLogger lambdaLogger = context.getLogger();
+ 
+        for (SQSEvent.SQSMessage message : sqsEvent.getRecords()) {
+            lambdaLogger.log(message.getBody());
+            logger.info(message.getBody());
+        }
+        return "Success";
+    }
 }
