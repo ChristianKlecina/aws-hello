f072ccf4302555f8d386a4d719453a8322226a8a
ChristianKlecina <djfntt@yahoo.com>
2024-07-08 19:31:30 +0200

    task05

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,18 +1,23 @@
 package com.task05;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+
 import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
-import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
+
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.resources.DependsOn;
@@ -20,10 +25,12 @@ import com.syndicate.deployment.model.*;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
-import java.time.Instant;
+
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -39,130 +46,110 @@ import java.util.UUID;
 @EnvironmentVariables(value = {
         @EnvironmentVariable(key = "region", value = "${region}"),
         @EnvironmentVariable(key = "table", value = "${target_table}")})
-public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class ApiHandler implements RequestHandler<Object, APIGatewayV2HTTPResponse> {
 
-    private final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
-    private final Table table = dynamoDB.getTable("Events");
+    //    private final String TABLE_NAME = "Events";
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(Object event, Context context) {
+        LambdaLogger lambdaLogger = context.getLogger();
         try {
-            Request req = objectMapper.readValue(request.getBody(), Request.class);
-            Event event = new Event(req.getPrincipalId(), req.getContent());
+            lambdaLogger.log("Lambda logger EVENT: " + objectMapper.writeValueAsString(event));
+            lambdaLogger.log("EVENT TYPE: " + event.getClass());
+            Map<String, Object> requestBody = objectMapper.readValue(objectMapper.writeValueAsString(event), LinkedHashMap.class);
+
             String id = UUID.randomUUID().toString();
+            int principalId = (Integer) requestBody.get("principalId");
+            Map<String, Object> content = (Map<String, Object>) requestBody.get("content");
+
+            lambdaLogger.log(content.toString());
+
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
             String createdAt = OffsetDateTime.now(ZoneOffset.UTC).format(formatter);
-            table.putItem(new PutItemSpec().withItem(new Item()
-                    .withString("id", id)
-                    .withInt("principalId", event.getPrincipalId())
-                    .withString("createdAt", createdAt)
-                    .withMap("body", event.getBody())));
+            lambdaLogger.log("Time: " + createdAt);
 
-            Response response = new Response(201, event);
-            APIGatewayProxyResponseEvent apiResponse = new APIGatewayProxyResponseEvent();
-            apiResponse.setStatusCode(201);
-            apiResponse.setBody(objectMapper.writeValueAsString(response));
-            return apiResponse;
+            // DYNAMODB
 
+            Map<String, AttributeValue> itemValues = getAttributesMap(id, principalId, content, createdAt);
+
+            Item item = new Item();
+            item.withString("id", id);
+            item.withInt("principalId", principalId);
+            item.withString("createdAt", createdAt);
+            item.withMap("body", content);
+
+            saveToDynamoDb(itemValues, item);
+
+
+            return createSuccessResponse(item);
         } catch (Exception e) {
+            lambdaLogger.log("Error: " + e.getMessage());
             e.printStackTrace();
-            return new APIGatewayProxyResponseEvent().withStatusCode(500).withBody(e.getMessage());
+            return APIGatewayV2HTTPResponse.builder()
+                    .withStatusCode(500)
+                    .withBody(e.getMessage())
+                    .build();
         }
     }
-}
 
-class Request {
-    private int principalId;
-    private Map<String, String> content;
+    private void saveToDynamoDb(Map<String, AttributeValue> itemValues, Item item) throws JsonProcessingException {
+        final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(System.getenv("region"))
+                .build();
 
-    // Getters and setters
-    public int getPrincipalId() {
-        return principalId;
+        try {
+            ddb.putItem(System.getenv("table"), itemValues);
+        } catch (ResourceNotFoundException e) {
+            System.err.format("Error: The table \"%s\" can't be found.\n", System.getenv("table"));
+            System.err.println(objectMapper.writeValueAsString(itemValues));
+            System.exit(1);
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 
-    public void setPrincipalId(int principalId) {
-        this.principalId = principalId;
+    private static Map<String, AttributeValue> getAttributesMap(String id,
+                                                                int principalId,
+                                                                Map<String, Object> content,
+                                                                String createdAt) {
+        Map<String, AttributeValue> itemValues = new HashMap<>();
+
+        AttributeValue principalIdAttribute = new AttributeValue();
+        principalIdAttribute.setN(String.valueOf(principalId));
+
+        AttributeValue contentAttribute = new AttributeValue();
+        Map<String, AttributeValue> contentAttributesMap = new HashMap<>();
+        for (Map.Entry entry : content.entrySet()) {
+            contentAttributesMap.put(String.valueOf(entry.getKey()), new AttributeValue((String) entry.getValue()));
+        }
+        contentAttribute.setM(contentAttributesMap);
+
+        itemValues.put("id", new AttributeValue(id));
+        itemValues.put("principalId", principalIdAttribute);
+        itemValues.put("createdAt", new AttributeValue(createdAt));
+        itemValues.put("body", contentAttribute);
+        return itemValues;
     }
 
-    public Map<String, String> getContent() {
-        return content;
+    private APIGatewayV2HTTPResponse createSuccessResponse(Item item) {
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("statusCode", 201);
+        responseBody.put("event", item.asMap());
+
+        String body;
+        try {
+            body = objectMapper.writeValueAsString(responseBody);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            body = null;
+        }
+
+        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+        response.setStatusCode(201);
+        response.setBody(body);
+        return response;
     }
 
-    public void setContent(Map<String, String> content) {
-        this.content = content;
-    }
-}
-
-class Response {
-    private int statusCode;
-    private Object event;
-
-    // Constructor, getters and setters
-    public Response(int statusCode, Object event) {
-        this.statusCode = statusCode;
-        this.event = event;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public Object getEvent() {
-        return event;
-    }
-
-    public void setEvent(Object event) {
-        this.event = event;
-    }
-}
-
-class Event {
-    private String id;
-    private int principalId;
-    private String createdAt;
-    private Map<String, String> body;
-
-    public Event(int principalId, Map<String, String> body) {
-        this.id = UUID.randomUUID().toString();
-        this.principalId = principalId;
-        this.createdAt = Instant.now().toString();
-        this.body = body;
-    }
-
-    // Getters and setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getPrincipalId() {
-        return principalId;
-    }
-
-    public void setPrincipalId(int principalId) {
-        this.principalId = principalId;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Map<String, String> getBody() {
-        return body;
-    }
-
-    public void setBody(Map<String, String> body) {
-        this.body = body;
-    }
 }
