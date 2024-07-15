0092e3af35008cde10b864b35052eccf5e094f01
ChristianKlecina <djfntt@yahoo.com>
2024-07-15 22:33:39 +0200

    initial

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,55 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import task08.WeatherForecast;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "api_handler",
+		roleName = "api_handler-role",
+		isPublishVersion = false,
+		layers = {"sdk-layer"}
+)
+@LambdaLayer(
+		layerName = "sdk-layer",
+		libraries = {"lib/meteo_api-1.0-SNAPSHOT.jar"},
+		runtime = DeploymentRuntime.JAVA11,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class ApiHandler implements RequestHandler<Object, String> {
+
+	public String handleRequest(Object request, Context context) {
+
+		WeatherForecast weatherForecast = new WeatherForecast();
+        try {
+            String weather = weatherForecast.getWeather();
+			return weather.replaceAll("\\\"", "\"");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+//        System.out.println("Hello from lambda");
+//		Map<String, Object> resultMap = new HashMap<String, Object>();
+//		resultMap.put("statusCode", 200);
+//		resultMap.put("body", "Hello from Lambda");
+//		return resultMap;
+	}
+}
